1a4e2b24f5b9647f9e5622d3ed8a8080117d3fdd
Iliyan Kiryakov <iliyankiryakovv@gmail.com>
2024-04-17 23:16:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//calculates the total amount of all expenses
 float calculateAllExpenses(float expenses[], int n) {
     float total = 0;
     for (int i = 0; i < n; i++) {
@@ -18,16 +19,16 @@ int main() {
     
 
     do {
-        printf("What's your monthly income?\n");
-        success = scanf("%f", &income);
+        printf("What's your monthly income?\n"); //asks the user for their income
+        success = scanf("%f", &income); //reads the user input and stores it in the variable
         
-        // Clear the input buffer in case of invalid input
+        //clears the input if its entered invalid input like letters
         while (getchar() != '\n');
 
         if (!success) {
-            printf("Invalid input. Please enter a number.\n");
+            printf("Invalid input. Please enter a number.\n"); //displays error message if invalid input is entered
         }
-    } while (!success);
+    } while (!success); //loops until a valid input like number is entered
 
     do {
         printf("Enter the number of your monthly expenses:\n");
@@ -39,8 +40,9 @@ int main() {
             printf("Invalid input. Please enter a number.\n");
         }
     } while (!success);
-    
-    float expenses[mExpenses];
+
+    //creates an array or more to store the monthly expenses entered by the user
+    float expenses[mExpenses]; 
     for (int i = 0; i < mExpenses; i++) {
     printf("Enter expense #%d: ", i+1);
     scanf("%f", &expenses[i]);
@@ -57,9 +59,10 @@ int main() {
         }
     } while (!success);
     
-    float totalExpenses = calculateAllExpenses(expenses, mExpenses);
-    float remainingBalance = income - totalExpenses - savings;
-    
+    float totalExpenses = calculateAllExpenses(expenses, mExpenses); //calculates the amount of the expenses
+    float remainingBalance = income - totalExpenses - savings; //calculates the remaining balance after substracting the expenses and savings from the income
+
+    //displays the expenses and remaining balance
     printf("\nYour total monthly expenses: %.2f\n", totalExpenses);
     printf("Your remaining budget after savings: %.2f\n", remainingBalance);
     
